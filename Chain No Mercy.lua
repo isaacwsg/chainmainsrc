02ce6a7b13e50c8411393b61931310096bc62b12
isaacwsg <153341038+isaacwsg@users.noreply.github.com>
2025-04-29 21:57:12 +0530

    Update Chain No Mercy.lua

--- Chain No Mercy.lua
+++ Chain No Mercy.lua
@@ -1,9 +1,9 @@
 local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
 
 local GUI = Mercury:Create{
-    Name = "⚔️Chain No Mercy⚔️",
+    Name = "Chain No Mercy",
     Size = UDim2.fromOffset(600, 400),
-Theme = Mercury.Themes.Dark,
+    Theme = Mercury.Themes.Dark,
     Link = "https://github.com/deeeity/mercury-lib"
 }
 
